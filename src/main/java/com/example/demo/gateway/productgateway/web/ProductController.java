a0acc4bf5c3f4268c080e7c435edd95be46197e0
Emine Yeter Ataseven <emine.yeter.ataseven@accenture.com>
2020-05-20 16:58:59 -0400

    changed version

--- src/main/java/com/example/demo/gateway/productgateway/web/ProductController.java
+++ src/main/java/com/example/demo/gateway/productgateway/web/ProductController.java
@@ -3,6 +3,7 @@ package com.example.demo.gateway.productgateway.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
@@ -17,35 +18,46 @@ public class ProductController  {
 
     Logger logger = LoggerFactory.getLogger(ProductController.class);
 
-    @Bean
-    @ConfigurationProperties("security.oauth2.client")
-    public ClientCredentialsResourceDetails oAuthDetails(){
+    @Value("#{ @environment['example.baseUrl'] }")
+    private String serverBaseUrl;
 
-        // provides oauth2 cleint deatils
-        ClientCredentialsResourceDetails config = new ClientCredentialsResourceDetails();
-        config.setClientId("tutorialspoint");
-        config.setClientSecret("my-secret-key");
-        config.setAccessTokenUri("http://localhost:8081/oauth/token");
-        config.setGrantType("client_credentials");
-
-        return config;
-    }
-
+    @Autowired
+    ClientCredentialsResourceDetails oAuthDetails;
 
     @Autowired
     RestTemplate restTemplate;
 
+//    @Bean
+//    @ConfigurationProperties("security.oauth2.client")
+//    public ClientCredentialsResourceDetails oAuthDetails(){
+//
+//        // provides oauth2 cleint deatils
+//        ClientCredentialsResourceDetails config = new ClientCredentialsResourceDetails();
+//        config.setClientId("tutorialspoint");
+//        config.setClientSecret("my-secret-key");
+//        config.setAccessTokenUri("http://localhost:8081/oauth/token");
+//        config.setGrantType("client_credentials");
+//
+//        return config;
+//    }
+
+    @Bean
+    @ConfigurationProperties("security.oauth2.client")
+    protected ClientCredentialsResourceDetails oAuthDetails() {
+        return new ClientCredentialsResourceDetails();
+    }
+
     @Bean
     public RestTemplate restTemplate(){
         // initialize OAuth2 Rest Client with configuration details
-        MyOAuth2RestTemplate template = new MyOAuth2RestTemplate(oAuthDetails());
+        MyOAuth2RestTemplate template = new MyOAuth2RestTemplate(oAuthDetails);
         template.setRetryBadAccessTokens(true);
         return template;
     }
 
     @GetMapping(path = "/product", produces = MediaType.APPLICATION_JSON_VALUE)
     Product getProduct(){
-        String url = "http://localhost:8081/products";
+        String url = serverBaseUrl + "/products";
         String result = restTemplate.getForObject(url, String.class);
         return new Product(result);
     }
